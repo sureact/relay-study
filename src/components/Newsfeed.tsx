792dcb9de65c1a1b3412d48843834db5c9372c9b
milk717 <robolindasoo@gmail.com>
2024-11-02 15:21:18 +0900

    :art: query를 inline으로 변경

--- src/components/Newsfeed.tsx
+++ src/components/Newsfeed.tsx
@@ -2,28 +2,26 @@ import * as React from "react";
 import Story from "./Story";
 import { graphql } from "relay-runtime";
 import { useLazyLoadQuery } from "react-relay";
-import type {NewsfeedQuery as NewsfeedQueryType  } from "./__generated__/NewsfeedQuery.graphql";
-
-const NewsfeedQuery = graphql`
- query NewsfeedQuery {
-    topStory {
-        title
-        summary
-        poster {
-            name
-            profilePicture {
-                url
-            }
-        }
-        thumbnail {
-            url
-        }
-    }
- }
-`
+import type {NewsfeedQuery   } from "./__generated__/NewsfeedQuery.graphql";
 
 export default function Newsfeed() {
-  const data = useLazyLoadQuery<NewsfeedQueryType>(NewsfeedQuery, {});
+  const data = useLazyLoadQuery<NewsfeedQuery>(graphql`
+      query NewsfeedQuery {
+          topStory {
+              title
+              summary
+              poster {
+                  name
+                  profilePicture {
+                      url
+                  }
+              }
+              thumbnail {
+                  url
+              }
+          }
+      }
+  `, {});
 
   const story = data.topStory;
 
